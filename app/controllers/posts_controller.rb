c22655997e5e264e50c710fb665f69373edd1e3e
Tomek Wielgocki <tomek@railwaymen.org>
2014-11-21 13:35:43 +0100

    Integrate parsing service with application

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+  def create
+    data_sources = DataSource.where(id: parser_params[:data_source_ids])
+    service = ParsingService.new(data_sources)
+    service.call
+    @posts = Post.where(data_source_id: data_sources.pluck(:id)).order(posted_at: :desc)
+    render :index
+  end
+
+  private
+
+  def parser_params
+    params.require(:parser).permit(data_source_ids: [])
+  end
 end
